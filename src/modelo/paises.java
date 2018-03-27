1c70bb5b5e8ed0c2c71c88b3d648fd1758fd32a3
gestevez <gestevez@CAECOPAZ02.caecopaz.mil.ar>
2018-03-27 10:05:45 -0300

    Filtrado de paises, Chequear si anda

--- src/modelo/paises.java
+++ src/modelo/paises.java
@@ -1,65 +1,73 @@
 package modelo;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class paises {
 	String pais;
 	boolean valores[];
+	static ArrayList<paises> listaPaises;
 
 	public paises(String pais, boolean[] valores) {
 		this.pais = pais;
 		this.valores = valores;
 	}
-	
-	public static ArrayList<paises> crearListaPaises(){
-		paises p ;
-		ArrayList<paises> listaPaises= new ArrayList<paises>();
-		
-		
-		boolean[] arg= {true,false, false, true, false, false, true ,false};
-		p = new paises("Argentina", arg);
-		listaPaises.add(p) ;
-		
-		boolean[] uru= {false,false, false, true, false, false, true ,false};
-		p = new paises("Uruguay", uru);
-		listaPaises.add(p) ;
-		
-		boolean[] bra= {false,true, false, true, false, false, true ,false};
-		p = new paises("Brasil", bra);
-		listaPaises.add(p) ;
 
-		boolean[] chi= {true,true, true, false, false, false, true ,false};
+	public static ArrayList<paises> crearListaPaises() {
+		paises p;
+		listaPaises = new ArrayList<paises>();
+
+		boolean[] arg = { true, false, false, true, false, false, true, false };
+		p = new paises("Argentina", arg);
+		listaPaises.add(p);
+
+		boolean[] uru = { false, false, false, true, false, false, true, false };
+		p = new paises("Uruguay", uru);
+		listaPaises.add(p);
+
+		boolean[] bra = { false, true, false, true, false, false, true, false };
+		p = new paises("Brasil", bra);
+		listaPaises.add(p);
+
+		boolean[] chi = { true, true, true, false, false, false, true, false };
 		p = new paises("Chile", chi);
-		listaPaises.add(p) ;
-		
-		
-		boolean[] per= {true,true, true, false, true, false, true ,false};
+		listaPaises.add(p);
+
+		boolean[] per = { true, true, true, false, true, false, true, false };
 		p = new paises("Peru", per);
-		listaPaises.add(p) ;
-		
-		
-		boolean[] bol= {true,false, false, false, false, false, true ,false};
+		listaPaises.add(p);
+
+		boolean[] bol = { true, false, false, false, false, false, true, false };
 		p = new paises("Bolvia", bol);
-		listaPaises.add(p) ;
-		
-		
-		boolean[] par= {false,true, false, false, false, false, true ,false};
+		listaPaises.add(p);
+
+		boolean[] par = { false, true, false, false, false, false, true, false };
 		p = new paises("Paraguay", par);
-		listaPaises.add(p) ;
-		
-		boolean[] col= {true,false, true, false, false, false, true ,false};
+		listaPaises.add(p);
+
+		boolean[] col = { true, false, true, false, false, false, true, false };
 		p = new paises("Colombia", col);
-		listaPaises.add(p) ;
-		
-		boolean[] ecu= {false,true, true, false, false, false, true ,false};
+		listaPaises.add(p);
+
+		boolean[] ecu = { false, true, true, false, false, false, true, false };
 		p = new paises("Ecuador", ecu);
-		listaPaises.add(p) ;
+		listaPaises.add(p);
 		return listaPaises;
-		
-		
-		
-		
+
 	}
-	
+
+	public static void filtrarPaises(boolean valor, int val) {
+		for (paises paises : listaPaises) {
+			if (paises.valores[val]!=valor)
+				listaPaises.remove(val);
+
+		}
+
+	}
+
+	@Override
+	public String toString() {
+		return "paises [pais=" + pais + ", valores=" + Arrays.toString(valores) + "]";
+	}
 
 }
